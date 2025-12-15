de745f3c6b86b517fd453ce1b9f1e7ef4940a26f
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2025-12-15 12:26:00 +0500

    Implement password generation feature and update input fields in the signup form

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-let body=document.querySelector("body")
+let body = document.querySelector("body")
+let passInput = document.getElementById("passInput")
+let randomPassVal = "hdip$%#356!rt&*"
+// console.log(randomPassVal.length);
 function changeColor(params) {
     body.classList.toggle("black")
+}
+function suggestPass(e) {
+    let pass = ""
+    console.log(`before ${pass}`);
+    for (let i = 0; i < 8; i++) {
+    pass+=randomPassVal[Math.floor(Math.random() * randomPassVal.length)]
+    }
+    e.preventDefault()
+    passInput.value=pass
  }
